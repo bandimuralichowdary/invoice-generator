c4b7d74e363fe461da3a43083cf0f98afc0b37b0
Bandi Murali Chowdary <97547174+bandimuralichowdary@users.noreply.github.com>
2025-11-27 19:15:45 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 import HomeClient from '../components/HomeClient';
 import { supabase } from '../lib/supabaseClient';
+export const dynamic = 'force-dynamic';
 
 export default function Home() {
   const params = useSearchParams();
